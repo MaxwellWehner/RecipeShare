c32df408b6e69f940c3aa45a9f322fc2cf17d8d6
Max Wehner <maxwehnerwork@gmail.com>
2022-06-23 07:35:08 -0700

    setup maven project

---
+++ Backend/src/test/java/com/maxwellwehner/recipeshare/RecipeshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxwellwehner.recipeshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
